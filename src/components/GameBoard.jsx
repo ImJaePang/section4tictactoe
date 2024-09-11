a327fd7472d9ca62491715f14cf9674e11ae1207
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2024-09-11 08:46:26 +0000

    배열에 접접근해서 변경하기

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null]
     ,[null, null, null]
@@ -6,15 +8,28 @@ const initialGameBoard = [
 
 
 export default function GameBoard(){
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSquare(rows, cols){
+
+        setGameBoard((prevGameBoard) => {
+            const presentGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            presentGameBoard[rows][cols] = "X";
+            return presentGameBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex)=>(
+            {gameBoard.map((row, rowIndex)=>(
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSelect, colIndex)=>(
                             <li key={colIndex}>
-                                <button>{playerSelect}</button>
-                                
+                                <button onClick={()=>handleSquare(rowIndex, colIndex)}>
+                                    {playerSelect}
+                                </button>
                             </li>
                         ))}
                     </ol>
