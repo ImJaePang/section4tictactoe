eacc4fd06f44efed9505dafa68121583e84cc24a
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2024-10-14 08:15:23 +0000

    에러남 다시 확인할 것

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log"
 import { useState } from "react";
+import {WINNING_COMBINATION} from "./components/winning-combination.js";
+
+const initialGameBoard = [
+  [null, null, null]
+  ,[null, null, null]
+  ,[null, null, null]
+];
 
 function symbolChange(theGame){
 
@@ -20,6 +27,16 @@ function App() {
   const [gamePlay, setGamePlay] = useState([]);
 
   let playerSymbol = symbolChange(gamePlay);
+
+
+  let gameBoard = initialGameBoard;
+
+  for(const gameTurn of gamePlay){
+      const {square, gamePlayer} = gameTurn;
+      const {rowIndex, colIndex} = square;
+
+      gameBoard[rowIndex][colIndex] = gamePlayer;
+  }
 
 
   function onHandleSquare(row, col){
@@ -47,7 +64,7 @@ function App() {
           <Player name="Player2" symbol="O" isHighlight={playerSymbol} />
           
         </ol>
-        <GameBoard onChangeSquare={onHandleSquare} gameTurns={gamePlay}/>
+        <GameBoard onChangeSquare={onHandleSquare} board={gameBoard}/>
       </div>
 
       <Log logs={gamePlay} />
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,39 +1,15 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null, null]
-    ,[null, null, null]
-    ,[null, null, null]
-];
 
 
-export default function GameBoard({onChangeSquare, gameTurns}){
 
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSquare(rows, cols){
-
-    //     setGameBoard((prevGameBoard) => {
-    //         const presentGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         presentGameBoard[rows][cols] = gamePlaySymbol;
-    //         return presentGameBoard;
-    //     });
-
-    //     handleChangeSymbol();
+export default function GameBoard({onChangeSquare, board}){
 
 
-    let gameBoard = initialGameBoard;
-
-    for(const gameTurn of gameTurns){
-        const {square, gamePlayer} = gameTurn;
-        const {rowIndex, colIndex} = square;
-
-        gameBoard[rowIndex][colIndex] = gamePlayer;
-    }
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex)=>(
+            {board.map((row, rowIndex)=>(
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSelect, colIndex)=>(
